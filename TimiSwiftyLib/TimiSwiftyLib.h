58a7dda81727de067bc8ed5403aa6f9d625e386b
Ogunsola oluwatimileyn <oyelekeogunsola@gmail.com>
2020-05-01 00:11:30 +0100

    Initial Commit

---
+++ TimiSwiftyLib/TimiSwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  TimiSwiftyLib.h
+//  TimiSwiftyLib
+//
+//  Created by user on 01/05/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TimiSwiftyLib.
+FOUNDATION_EXPORT double TimiSwiftyLibVersionNumber;
+
+//! Project version string for TimiSwiftyLib.
+FOUNDATION_EXPORT const unsigned char TimiSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimiSwiftyLib/PublicHeader.h>
+
+
